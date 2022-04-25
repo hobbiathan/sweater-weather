97bd07ebb5e8241743fddf22ec4201cb5ebf45a2
Ruben Pasillas <hobbiathan@gmail.com>
2022-04-25 09:54:50 -0600

    create munchie tests

---
+++ spec/requests/munchies_request_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Munchies API' do
+  context 'valid query, highest level keys' do
+    before { get "/api/v1/munchies?start=denver,co&destination=pueblo,co&food=chinese" }
+
+    it 'is returns 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'has appropriate json keys' do
+      expect(json[:data].keys).to eq([:id, :type, :attributes])
+    end
+
+    it 'has appropriate attributes' do
+      expect(json[:data][:attributes].keys).to eq([:destination_city, :travel_time, :forecast, :restaurant])
+    end
+
+    it 'has appropriate sub-keys FOR :forecast' do
+      expect(json[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+    end
+
+    it 'has appropriate sub-keys FOR :restaurant' do
+      expect(json[:data][:attributes][:forecast].keys).to eq([:name, :address])
+    end
+  end
+
+  context 'invalid query' do
+    it 'returns 400' do
+      expect(response).to have_http_status(400)
+    end
+
+    it 'renders custom error json' do
+        expect(json[:error]).to eq({:message=>"invalid parameters - no location provided"})
+    end
+  end
+end
