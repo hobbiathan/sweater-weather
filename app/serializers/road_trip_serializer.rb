4a4bba0c1fee34113103c5885b5cf56318e95267
Ruben Pasillas <hobbiathan@gmail.com>
2022-04-27 11:09:47 -0600

    finish road_trip

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,19 +1,19 @@
 class RoadTripSerializer
-  def self.trip_details()
+  def self.trip_details(origin, routePoro, weatherPoro)
     {
       "data": {
 
-        "id": null,
+        "id": nil,
         "type": "roadtrip",
         "attributes": {
 
-          "start_city": ,
-          "end_city:": ,
-          "travel_time": ,
+          "start_city": origin,
+          "end_city:": routePoro.destination_city,
+          "travel_time": routePoro.travel_time,
           "weather_at_eta": {
 
-            "temperature": ,
-            "conditions":
+            "temperature": weatherPoro.temperature,
+            "conditions": weatherPoro.conditions
 
           }
 
